04f0c16d146c208f1a00ad36bb8736846e78a5e7
Robin Nolte <nolte@uni-bremen.de>
2022-02-16 17:53:00 +0100

    Started work on the automatic generation project for SOMA Food

--- SOMA Food/src/main/java/SomaFoodGenerator.java
+++ SOMA Food/src/main/java/SomaFoodGenerator.java
@@ -1,4 +1,10 @@
+import org.jetbrains.annotations.NotNull;
 import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.modularity.ModuleExtractor;
+import org.semanticweb.owlapi.modularity.locality.LocalityClass;
+import org.semanticweb.owlapi.modularity.locality.SyntacticLocalityModuleExtractor;
+
+import java.util.Objects;
 
 public class SomaFoodGenerator {
 
@@ -6,10 +12,23 @@ public class SomaFoodGenerator {
 
     private final OWLOntology somaFood;
 
-    public SomaFoodGenerator(OWLOntology foodOn, OWLOntology somaFood) {
+    private final ModuleExtractor moduleExtractor;
+
+    public SomaFoodGenerator(@NotNull OWLOntology foodOn, @NotNull OWLOntology somaFood) {
+        Objects.requireNonNull(foodOn);
+        Objects.requireNonNull(somaFood);
+
         this.foodOn = foodOn;
         this.somaFood = somaFood;
+
+        moduleExtractor =  new SyntacticLocalityModuleExtractor(LocalityClass.STAR, foodOn.axioms());
     }
 
+    public OWLOntology getFoodOn() {
+        return foodOn;
+    }
 
+    public OWLOntology getSomaFood() {
+        return somaFood;
+    }
 }
