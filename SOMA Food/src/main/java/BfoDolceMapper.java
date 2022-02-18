cd61af745dd9fb7d3717b557cc5e1c11d24323b6
Robin Nolte <nolte@uni-bremen.de>
2022-02-18 18:32:58 +0100

    Worked on the import of Food

--- SOMA Food/src/main/java/BfoDolceMapper.java
+++ SOMA Food/src/main/java/BfoDolceMapper.java
@@ -1,25 +1,36 @@
-import org.apache.commons.lang3.NotImplementedException;
 import org.jetbrains.annotations.NotNull;
 import org.semanticweb.owlapi.model.OWLEntity;
 
-import java.util.Objects;
-import java.util.Optional;
+import java.util.*;
 
 public class BfoDolceMapper {
 
-    public BfoDolceMapper() {
+    private enum MAPPING_RELATION {
+        EQUIVALENT, SUBCLASS
     }
 
-    public boolean needsMapping(@NotNull OWLEntity toCheck) {
+    public record Translation(OWLEntity translated, MAPPING_RELATION relation) {
+    }
+
+    private final static Map<OWLEntity, Translation> translations;
+
+    static {
+        var tmpTranslation = new HashMap<OWLEntity, Translation>();
+
+        translations = Collections.unmodifiableMap(tmpTranslation);
+    }
+
+    private BfoDolceMapper() {
+    }
+
+    public static boolean needsMapping(@NotNull OWLEntity toCheck) {
         Objects.requireNonNull(toCheck);
-
-        throw new NotImplementedException();
+        return translations.containsKey(toCheck);
     }
 
-    public @NotNull Optional<OWLEntity> mapToDolce(@NotNull OWLEntity toMap) {
-        Objects.requireNonNull(toMap);
-
-        throw new NotImplementedException();
+    public static @NotNull Optional<Translation> translateToDolce(@NotNull OWLEntity toTranslate) {
+        Objects.requireNonNull(toTranslate);
+        return Optional.ofNullable(translations.get(toTranslate));
     }
 
 
--- SOMA Food/src/main/java/SomaFoodGenerator.java
+++ SOMA Food/src/main/java/SomaFoodGenerator.java
@@ -1,34 +1,81 @@
+import org.apache.commons.lang3.NotImplementedException;
 import org.jetbrains.annotations.NotNull;
-import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.HermiT.ReasonerFactory;
+import org.semanticweb.owlapi.model.*;
 import org.semanticweb.owlapi.modularity.ModuleExtractor;
 import org.semanticweb.owlapi.modularity.locality.LocalityClass;
 import org.semanticweb.owlapi.modularity.locality.SyntacticLocalityModuleExtractor;
+import org.semanticweb.owlapi.reasoner.OWLReasoner;
 
+import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-public class SomaFoodGenerator {
+public final class SomaFoodGenerator {
 
-    private final OWLOntology foodOn;
+	private final OWLOntology foodOn;
 
-    private final OWLOntology somaFood;
+	private final OWLOntology somaFood;
 
-    private final ModuleExtractor moduleExtractor;
+	private final ModuleExtractor moduleExtractor;
 
-    public SomaFoodGenerator(@NotNull OWLOntology foodOn, @NotNull OWLOntology somaFood) {
-        Objects.requireNonNull(foodOn);
-        Objects.requireNonNull(somaFood);
+	private final OWLReasoner reasoner;
 
-        this.foodOn = foodOn;
-        this.somaFood = somaFood;
+	public SomaFoodGenerator(@NotNull OWLOntology foodOn, @NotNull OWLOntology somaFood) {
+		Objects.requireNonNull(foodOn);
+		Objects.requireNonNull(somaFood);
 
-        moduleExtractor = new SyntacticLocalityModuleExtractor(LocalityClass.STAR, foodOn.axioms());
-    }
+		this.foodOn = foodOn;
+		this.somaFood = somaFood;
 
-    public OWLOntology getFoodOn() {
-        return foodOn;
-    }
+		moduleExtractor = new SyntacticLocalityModuleExtractor(LocalityClass.STAR, foodOn.axioms());
+		reasoner = new ReasonerFactory().createReasoner(foodOn);
+	}
 
-    public OWLOntology getSomaFood() {
-        return somaFood;
-    }
+	public OWLOntology getFoodOn() {
+		return foodOn;
+	}
+
+	public OWLOntology getSomaFood() {
+		return somaFood;
+	}
+
+
+	public void importEntities(@NotNull List<OWLEntity> toImport, boolean includeSiblings) {
+		Objects.requireNonNull(toImport);
+
+		var toImportStream = toImport.stream();
+
+		if (includeSiblings) {
+			toImportStream = toImportStream.flatMap(next -> siblings(next));
+		}
+
+		var relevantAxiomsStream = moduleExtractor.extract(toImportStream).toList();
+
+		var toMap = relevantAxiomsStream.stream().flatMap(HasSignature::signature).
+				filter(BfoDolceMapper::needsMapping).collect(Collectors.toUnmodifiableSet());
+
+		// TODO sort into SOMA FOOD
+		throw new NotImplementedException();
+	}
+
+	/**
+	 * Returns the (inferred) siblings of the given {@link OWLEntity}, including itself.
+	 * This method supports {@link OWLClassExpression}s, {@link OWLDataProperty}s, and {@link OWLObjectProperty}s.
+	 *
+	 * @param entity The {@link OWLEntity} whose siblings should be returned
+	 * @return The siblings of the given {@link OWLEntity}, including itself
+	 */
+	private @NotNull Stream<? extends OWLEntity> siblings(@NotNull final OWLEntity entity) {
+		return switch (entity) {
+			case OWLClassExpression classExpression -> reasoner.superClasses(classExpression, true).
+					flatMap(next -> reasoner.subClasses(next, true));
+			case OWLObjectProperty objectProperty -> reasoner.superObjectProperties(objectProperty, true).
+					flatMap(next -> (Stream<? extends OWLEntity>) reasoner.subObjectProperties(next, true));
+			case OWLDataProperty dataProperty -> reasoner.superDataProperties(dataProperty, true).
+					flatMap(next -> reasoner.subDataProperties(next, true));
+			default -> throw new IllegalArgumentException("Unexpected value: " + entity);
+		};
+	}
 }
